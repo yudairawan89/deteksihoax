ebf99cb00f78cecf9f7c23e7e8aeee211a23e488
yudairawan89 <yudairawan89@gmail.com>
2025-06-29 16:43:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ vectorizer = joblib.load("tfidf_vectorizer.pkl")
 # Logo
 col1, col2, col3 = st.columns([1, 6, 1])
 with col1:
-    st.image("kemdikbud.png", width=100)
+    st.image("kemendikbud.png", width=100)
 with col2:
     st.markdown("<h1 style='text-align: center; color: navy;'>Deteksi Berita Hoax</h1>", unsafe_allow_html=True)
     st.markdown("<h5 style='text-align: center; color: gray;'>Menggunakan Machine Learning Logistic Regression</h5>", unsafe_allow_html=True)
